31b597b81dc5eb21ee540f59282930f6d7a503ed
eiwashita <erikoiwashita@gmail.com>
2026-05-19 21:13:22 -0600

    Update SignUpPage, LoginPage, and Navbar
    - Make signup, login, and logout functional
    - Add AuthImagePattern component

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios.js';
+import toast from 'react-hot-toast';
 
 export const useAuthStore = create(set => ({
   // Initial state
@@ -19,6 +20,42 @@ export const useAuthStore = create(set => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async data => {
+    set({ isSigningUp: true });
+    try {
+      const response = await axiosInstance.post('/auth/signup', data);
+      set({ authUser: response.data });
+      toast.success('Account created successfully');
+    } catch (err) {
+      toast.error(err.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async data => {
+    set({ isLoggingIn: true });
+    try {
+      const response = await axiosInstance.post('/auth/login', data);
+      set({ authUser: response.data });
+      toast.success('Logged in successfully');
+    } catch (err) {
+      toast.error(err.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post('/auth/logout');
+      set({ authUser: null });
+      toast.success('Logged out successfully');
+    } catch (err) {
+      toast.error(err.response.data.message);
     }
   },
 }));
