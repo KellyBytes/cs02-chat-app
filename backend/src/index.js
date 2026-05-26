dbd629a2964b002a4607780b707d2dc9f1d96261
eiwashita <erikoiwashita@gmail.com>
2026-05-25 21:00:35 -0600

    Update files for production
    - Add package.json to project root
    - Update index.js in backend
    - Update base urls in axios.js and useAuthStore.js in frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,13 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
+import path from 'path';
 
 dotenv.config();
 // const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // backend
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +25,13 @@ app.use(
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
 
 // Use server.listen instead of app.listen
 server.listen(PORT, () => {
