23a9630f46a10c6f6f51cbd36a465e40284d7757
eiwashita <erikoiwashita@gmail.com>
2026-05-25 22:14:24 -0600

    Update files for deployment
    - Update backend/src/index.js to deploy with Railway
    - Update axios.js and useAuthStore.js to deploy with Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,32 +6,23 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
-import path from 'path';
 
 dotenv.config();
 // const app = express();
 
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve(); // backend
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   }),
 );
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-  });
-}
 
 // Use server.listen instead of app.listen
 server.listen(PORT, () => {
