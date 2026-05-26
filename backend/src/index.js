4faa3e12334b9039a1895a5cfddf6a256a567b40
eiwashita <erikoiwashita@gmail.com>
2026-05-25 23:21:41 -0600

    Fix Railway CORS Error

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,12 +11,14 @@ dotenv.config();
 // const app = express();
 
 const PORT = process.env.PORT || 5001;
+const allowedOrigins =
+  process.env.FRONTEND_URL.split(',') || 'http://localhost:5173';
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true,
   }),
 );
